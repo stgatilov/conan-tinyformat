561695a568bbc401e0fdadec13f5c547340b76c9
stgatilov <none@none>
2022-03-18 09:54:53 +0700

    Extracted version and source URL to conandata.yml.

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,8 @@
 from conans import ConanFile, CMake, tools
 import os
 
-
 class TinyformatConan(ConanFile):
     name = "tinyformat"
-    version = "2.1.0"
     description = "A minimal type safe printf() replacement"
     topics = ("format", "printf")
     homepage = "https://github.com/c42f/tinyformat"
@@ -13,8 +11,8 @@ class TinyformatConan(ConanFile):
     url = "https://github.com/stgatilov/conan-tinyformat"
 
     def source(self):
-        source_url = "https://github.com/c42f/tinyformat"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        # Download and extract tag tarball from github
+        tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = "tinyformat-" + self.version
         os.rename(extracted_dir, "sources");
 
